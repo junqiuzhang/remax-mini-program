ecb691f76a37a5b2f79d503814187365f0e7f456
zhangjunqiu <zhangjunqiu@beisen.com>
2019-12-22 15:00:49 +0800

    feat(app)config

--- src/app.config.js
+++ src/app.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pages: ['pages/index/index'],
   window: {
-    navigationBarTitleText: 'Remax Wechat Template',
-    navigationBarBackgroundColor: '#282c34'
+    navigationBarTitleText: 'Love zxz',
+    navigationBarBackgroundColor: '#ffffff'
   }
 };
