7a227cef7dda58f3c66e9e2fa21542313fdf31d3
zhangjunqiu <zhangjunqiu@beisen.com>
2020-01-10 19:10:00 +0800

    feat(msg)data

--- dist/functions/get-msg/index.js
+++ dist/functions/get-msg/index.js
@@ -5,6 +5,7 @@ const { NlpManager } = require('node-nlp')
 
 const zh = 'zh'
 const manager = new NlpManager({ languages: [zh] });
+manager.load(path.resolve('./data.json'));
 
 cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
@@ -14,7 +15,6 @@ cloud.init({
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
 
-  manager.load(path.resolve('./data.json'));
   const response = await manager.process(zh, event.message);
 
   return {
