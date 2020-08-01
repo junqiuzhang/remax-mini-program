988b39925609b512d3d3891ecc5603c9c7079dec
zhangjunqiu <zhangjunqiu@beisen.com>
2020-08-01 16:33:17 +0800

    perf(remax)升级

---
+++ src/data/index.ts
@@ -0,0 +1,7 @@
+export const PictureUrl = 'cloud://love-zxz.6c6f-love-zxz-1300970134/picture/'
+export const PictureCount = 5;
+export const WriteComment = '快把想对老公说的话写下来～';
+export const WriteCommentDone = '说完了～';
+export const UploadPicture = '上传';
+export const NextPicture = '下一张';
+export const Send = '发送';
