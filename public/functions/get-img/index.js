ea0c480b2ad057fb7a3cbba4ac06088e9bb11752
zhangjunqiu <zhangjunqiu@beisen.com>
2020-08-07 15:54:31 +0800

    fix(function)异常处理

--- public/functions/get-img/index.js
+++ public/functions/get-img/index.js
@@ -1,20 +1,38 @@
 // 云函数入口文件
-const cloud = require('wx-server-sdk')
+const cloud = require('wx-server-sdk');
 
 cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
-})
+});
 
-const db = cloud.database()
+const db = cloud.database();
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  const { data } = await db.collection('picture').get()
+  let log, wxContext, resp;
+
+  try {
+    log = cloud.logger();
+  } catch (error) {
+    log.error({ from: 'log error', error });
+  }
+
+  try {
+    wxContext = cloud.getWXContext();
+  } catch (error) {
+    log.error({ from: 'get wxContext error', error });
+  }
+
+  try {
+    resp = await db.collection('picture').get();
+  } catch (error) {
+    log.error({ from: 'get picture error', error });
+  }
+
   return {
-    response: data,
+    response: resp.data,
     openid: wxContext.OPENID,
     appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
-}
+    unionid: wxContext.UNIONID
+  };
+};
--- public/functions/get-msg/index.js
+++ public/functions/get-msg/index.js
@@ -1,47 +1,72 @@
 // 云函数入口文件
 const path = require('path');
-const cloud = require('wx-server-sdk')
-const { NlpManager } = require('node-nlp')
-
-const zh = 'zh'
-const manager = new NlpManager({ languages: [zh] });
-manager.load(path.resolve('./data.json'));
+const cloud = require('wx-server-sdk');
+const { NlpManager } = require('node-nlp');
 
 cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
-})
+});
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  // const fileID = 'cloud://love-zxz.6c6f-love-zxz-1300970134/data/data.json'
-  // const { fileContent } = await cloud.downloadFile({ fileID })
-  // const fileData = fileContent.toString('utf8')
-  // manager.import(fileData)
+  let log, wxContext, manager, question, response, regQuestion, respAnswer;
 
-  const question = event.message;
-  const response = await manager.process(zh, question)
-
-  const regQuestion = new RegExp(question.split('').join('\\w+'))
-  const ans = response.answers.find(({ answer }) => {
-    const regAnswer = new RegExp(answer.split('').join('\\w+'))
-    return regQuestion.test(answer) || regAnswer.test(question) || question.includes(answer) || answer.includes(question)
-  })
-  if (ans && ans.answer) {
-    response.answer = ans.answer
+  try {
+    log = cloud.logger();
+  } catch (error) {
+    log.error({ from: 'log error', error });
   }
 
-  // const log = cloud.logger()
-  // log.info({
-  //   fileID,
-  //   fileContent,
-  //   fileData
-  // })
-  
+  try {
+    wxContext = cloud.getWXContext();
+  } catch (error) {
+    log.error({ from: 'get wxContext error', error });
+  }
+
+  try {
+    manager = new NlpManager({ languages: ['zh'] });
+    manager.load(path.resolve('./data.json'));
+  } catch (error) {
+    log.error({ from: 'manager create or load error', error });
+  }
+
+  try {
+    question = event.message;
+  } catch (error) {
+    log.error({ from: 'get param error', error });
+  }
+
+  try {
+    response = await manager.process('zh', question);
+  } catch (error) {
+    log.error({ from: 'manager process error', error });
+  }
+
+  try {
+    regQuestion = new RegExp(question.split('').join('\\w+'));
+    respAnswer = response.answers.find(({ answer }) => {
+      const regAnswer = new RegExp(answer.split('').join('\\w+'));
+      return (
+        regQuestion.test(answer) ||
+        regAnswer.test(question) ||
+        question.includes(answer) ||
+        answer.includes(question)
+      );
+    });
+  } catch (error) {
+    log.error({ from: 'answer select error', error });
+  }
+
+  try {
+    response.answer = (respAnswer && respAnswer.answer) || '😂';
+  } catch (error) {
+    log.error({ from: 'set answer error', error });
+  }
+
   return {
     response,
     openid: wxContext.OPENID,
     appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
-}
+    unionid: wxContext.UNIONID
+  };
+};
