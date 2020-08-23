261227a1231d65607d1f086ca167fdc3cb77f771
zhangjunqiu <zhangjunqiu@beisen.com>
2020-08-23 17:24:14 +0800

    fix(msg)scroll

--- src/pages/talk/index.js
+++ src/pages/talk/index.js
@@ -20,7 +20,7 @@ function getDialogPipe(value) {
       resolve(msg.answer);
     });
     return;
-  })
+  });
 }
 function renderDialogPipe(props) {
   return <Text className={'text'}>{props}</Text>;
@@ -30,23 +30,23 @@ export default () => {
   const [value, setValue] = React.useState('');
   const [disabled, setDisabled] = React.useState(true);
   const [snowIcon, setSnowIcon] = React.useState('');
-  const handleChange = (e) => {
+  const handleChange = React.useCallback(e => {
     if (e.detail.value.trim().length < 1 || e.detail.value.length > 15) {
       setDisabled(true);
     } else {
       setDisabled(false);
       setValue(e.detail.value);
     }
-  }
+  }, []);
   const setDialog = (ques, ans) => {
     const question = {
       isAnswer: false,
       value: ques
-    }
+    };
     const answer = {
       isAnswer: true,
       value: ans
-    }
+    };
     let nList = [];
     if (ans) {
       nList = [...list, question, answer];
@@ -55,50 +55,69 @@ export default () => {
     }
     setValue('');
     setList(nList);
-  }
-  const setSnow = (value) => {
+    scrollToBottom();
+  };
+  const setSnow = React.useCallback(value => {
     let icon = '';
-    Object
-    .keys(SnowIcon)
-    .forEach(val => {
+    Object.keys(SnowIcon).forEach(val => {
       if (value.includes(val)) {
-        icon = SnowIcon[val]
+        icon = SnowIcon[val];
       }
-    })
+    });
     if (icon) {
       setSnowIcon(icon);
       setTimeout(() => {
         setSnowIcon('');
-      }, 10000)
+      }, 10000);
     }
-  }
+  }, []);
+  const scrollToBottom = React.useCallback(() => {
+    const query = wx.createSelectorQuery();
+    query.selectViewport().scrollOffset();
+    query.select('.list-wrap').boundingClientRect();
+    query.exec(function (res) {
+      const top = res[0].scrollHeight || 999;
+      wx.pageScrollTo({
+        scrollTop: top,
+        duration: 300
+      });
+    });
+  }, []);
   const handleClickButton = async () => {
     setDisabled(true);
     setDialog(value);
-    
+
     // snow特效
     setSnow(value);
 
     const dialog = await getDialogPipe(value);
     setDialog(value, dialog);
-  }
+  };
   return (
     <View className={'talk-page'}>
-      {
-        snowIcon && <SnowFall icon={snowIcon} />
-      }
+      {snowIcon && <SnowFall icon={snowIcon} />}
       <View className={'list-wrap'}>
-        {
-          list.map(({ isAnswer, value }, i) => <View className={`list-item ${isAnswer ? `answer bounce-in-left` : `bounce-in-right`}`} key={i}>
+        {list.map(({ isAnswer, value }, i) => (
+          <View
+            className={`list-item ${
+              isAnswer ? `answer bounce-in-left` : `bounce-in-right`
+            }`}
+            key={i}
+          >
             {renderDialogPipe(value)}
-          </View>)
-        }
+          </View>
+        ))}
       </View>
       <View className={'bottom-wrap'}>
-        <Input className={'input'} value={value} onInput={handleChange}/>
-        <Button className={`button ${disabled && `disabled`}`} disabled={disabled} onClick={handleClickButton}>{Send}</Button>
+        <Input className={'input'} value={value} onInput={handleChange} />
+        <Button
+          className={`button ${disabled && `disabled`}`}
+          disabled={disabled}
+          onClick={handleClickButton}
+        >
+          {Send}
+        </Button>
       </View>
     </View>
   );
 };
-
